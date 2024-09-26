1aa9ba4386971d82f1e9a82852f5adbd9b7fddb2
okupoluyi <gbenga.kupoluyi@gmail.com>
2024-09-26 14:52:45 -0400

    Adds Switchcalculator and TwoDimemsionalArray

---
+++ src/main/java/com/bptn/course/_04_arrays/TwoDimensionalArray.java
@@ -0,0 +1,26 @@
+package com.bptn.course._04_arrays;
+
+public class TwoDimensionalArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	//int [][] numbers = new  int[3][2];
+		
+		int [][] number = {
+				{85,90},
+				{88,92},
+				{70,65}
+		};
+	
+
+		if (number[0] != number[2]) {
+		 System.out.println();
+		 
+		 else {
+			 
+		 }
+		}
+	}
+
+}
---
+++ src/main/java/com/bptn/course/_05_strings/SwitchCalculator.java
@@ -0,0 +1,48 @@
+package com.bptn.course._05_strings;
+
+import java.util.Scanner;
+
+public class SwitchCalculator {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int choice;
+		double billAmount = 0.0;
+		do {
+		
+		
+		System.out.println("Please choose an item from the list: ");
+		System.out.println("1. Pizza - $12.99");
+		System.out.println("2. Burger - $8.99");
+		System.out.println("3. Pasta - $9.99");
+		System.out.println("4. Salad - $6.99");
+		System.out.println("5. Exit");
+	    
+		 choice = scanner.nextInt();
+		 
+		 switch(choice) {
+		 case 1: System.out.println("You ordered Pizza");
+		    billAmount +=12.99;
+		    break;
+		 case 2: System.out.println("You ordered Burger");
+		    billAmount +=8.99; 
+		    break;
+		  case 3: System.out.println("You ordered Pasta");
+		    billAmount +=9.99;
+		    break;
+		  case 4: System.out.println("You ordered Salad");
+		    billAmount +=6.99;
+		    break;
+		  case 5: if (billAmount >0) {
+			  System.out.println("Thank you for ordering");
+		  }
+		 
+		 }
+		 while {
+		 }
+
+	}
+
+}
