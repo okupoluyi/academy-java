440fd4f52a56166826cc2719c6ba51945464c0d4
okupoluyi <gbenga.kupoluyi@gmail.com>
2024-09-26 00:06:28 -0400

    Adding some files I worked on for string

---
+++ src/main/java/com/bptn/course/_05_strings/StringChallege.java
@@ -0,0 +1,31 @@
+package com.bptn.course._05_strings;
+
+public class StringChallege {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * //Read User Input Scanner scanner = new Scanner(System.in); String input =
+		 * scanner.nextLine();
+		 * 
+		 * //Declare storage variables int vowelCount =0; int consonantCount = 0;
+		 * 
+		 * //Convert everything to lowercase String lowerCaseInput =
+		 * input.toLowerCase();
+		 * 
+		 * //Loop through each character in the string for(int i=0;
+		 * i<lowerCaseInput.length(); i++) { char ch = lowerCaseInput.charAt(i);
+		 * 
+		 * //Check if its vowel or consonant if (ch=='a'||
+		 * ch=='e'||ch=='i'||ch=='o'||ch=='u'){ System.out.println("This is a vowel"); }
+		 * else (){
+		 * 
+		 * } }
+		 * 
+		 * System.out.print("");
+		 * 
+		 */
+	}
+
+}
--- src/main/java/com/bptn/course/_05_strings/StringDemo.java
+++ src/main/java/com/bptn/course/_05_strings/StringDemo.java
@@ -3,7 +3,22 @@ package com.bptn.course._05_strings;
 public class StringDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+	
+		String userName = "testuser";
+		
+		System.out.println(userName      );
+		
+		System.out.println(userName.length());
+		System.out.println(userName.charAt(2));
+		System.out.println(userName.substring(4));
+		System.out.println(userName.substring(2,4));
+		System.out.println(userName.equals("testUser"));
+		System.out.println(userName.trim());
+		System.out.println(userName.indexOf('r'));
+		
+		
+
 
 	}
 
