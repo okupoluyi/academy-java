3da4595604351cf8f11dfed11d4591c943d74deb
okupoluyi <gbenga.kupoluyi@gmail.com>
2024-10-16 08:10:04 -0400

    all pending files

---
+++ src/main/java/src/main/java/com/bptn/course/_01_variables/Variables.java
@@ -0,0 +1,10 @@
+package src.main.java.com.bptn.course._01_variables;
+
+public class Variables {
+
+	public static void main(String[] args) {
+
+		System.out.print("Hello!");
+
+	}
+}
---
+++ src/main/java/src/main/java/com/bptn/course/_05_strings/Strings.java
@@ -0,0 +1,10 @@
+package src.main.java.com.bptn.course._05_strings;
+
+public class Strings {
+
+	public static void main(String[] args) {
+		// Test
+		// This is a test
+
+	}
+}
